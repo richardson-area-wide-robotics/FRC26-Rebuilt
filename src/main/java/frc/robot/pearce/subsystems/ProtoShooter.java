486d421972a2e1fd969b47ee880991e978d44c5d
strubium <113206902+strubium@users.noreply.github.com>
2026-01-17 10:46:18 -0600

    Update WPI, remove motor2 from ProtoShooter

--- src/main/java/frc/robot/pearce/subsystems/ProtoShooter.java
+++ src/main/java/frc/robot/pearce/subsystems/ProtoShooter.java
@@ -1,37 +1,24 @@
 package frc.robot.pearce.subsystems;
 
-import com.revrobotics.spark.SparkBase;
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel;
 import com.revrobotics.spark.config.SparkBaseConfig;
-import com.revrobotics.spark.config.SparkFlexConfig;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.common.components.EasyMotor;
 import frc.robot.common.subsystems.DashboardSubsystem;
 
 public class ProtoShooter extends DashboardSubsystem {
 
     private SparkFlex motor1;
-    private SparkFlex motor2;
-
 
     public ProtoShooter(int id1, int id2){
-
-
-         //motor2 = EasyMotor.createEasySparkFlex(id2, SparkLowLevel.MotorType.kBrushless, SparkBaseConfig.IdleMode.kCoast);
          motor1 = EasyMotor.createEasySparkFlex(id1, SparkLowLevel.MotorType.kBrushless, SparkBaseConfig.IdleMode.kCoast);
-
     }
 
     public void runShooter(){
-
         motor1.set(2);
-        //motor2.set(2);
     }
 
     public void stopShooter(){
         motor1.set(0);
-        //motor2.set(0);
-
     }
 }
