940b4e4846910e2a980754f5fa7c2c078ac937e4
at384752 <at384752@g.risd.org>
2026-01-20 15:27:39 -0600

    Delete STEM Practicum container

--- src/main/java/frc/robot/common/swerve/RAWRSwerveModule.java
+++ src/main/java/frc/robot/common/swerve/RAWRSwerveModule.java
@@ -124,12 +124,7 @@ public class RAWRSwerveModule extends SwerveModule implements Sendable {
               Dimensionless.ofBaseUnits(1.3, Units.Value)), 
             ZERO_OFFSET.get(location),
             PIDConstants.of(0.18, 0, 0.174, 0, 0), // Replace with actual PID constants
-              FFConstants.of(
-                      0,
-                      0,
-                      0,
-                      0
-              ),
+            FFConstants.of(0, 0, 0, 0),
           PIDConstants.of(4.55, 0, 0.2, 0, 0), // The PID for the rotate Motor
           FFConstants.of(0, 0, 0, 0),  // Replace with actual feed-forward constants
           Dimensionless.ofBaseUnits(CommonConstants.DriveConstants.DRIVE_SLIP_RATIO, Units.Value),
