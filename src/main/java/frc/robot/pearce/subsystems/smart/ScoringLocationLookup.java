006db50521271695c99b71dc7e58dc772b98b683
strubium <113206902+strubium@users.noreply.github.com>
2026-02-21 15:01:09 -0600

    yeah!

--- src/main/java/frc/robot/pearce/subsystems/smart/ScoringLocationLookup.java
+++ src/main/java/frc/robot/pearce/subsystems/smart/ScoringLocationLookup.java
@@ -2,28 +2,41 @@ package frc.robot.pearce.subsystems.smart;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.DriverStation;
 import frc.robot.pearce.components.RobotSector;
 import lombok.AllArgsConstructor;
 
 public class ScoringLocationLookup {
-    private ScoringLocation scoringLocations[] = new ScoringLocation[999];
+    private ScoringLocationFinalized scoringLocations[] = new ScoringLocationFinalized[999];
 
 
-    public ScoringLocation[] buildScoreingLocations(){
-        ScoringLocation scoringLocations[] = new ScoringLocation[999];
+    public void buildScoreingLocations(){
+        ScoringLocation scoringLocationsRaw[] = new ScoringLocation[999];
 
 
-        scoringLocations[0] = new ScoringLocation("hub", new Pose2d(13,4, new Rotation2d()), null);
-        scoringLocations[1] = new ScoringLocation("left_corner", new Pose2d(16,0.5, new Rotation2d()), null);
-        scoringLocations[2] = new ScoringLocation("climber", new Pose2d(14.5,4, new Rotation2d()), null);
-        scoringLocations[3] = new ScoringLocation("right_corner", new Pose2d(16,7.5, new Rotation2d()), null);
-        scoringLocations[4] = new ScoringLocation("rightside_hub", new Pose2d(13,5, new Rotation2d()), null);
-        scoringLocations[4] = new ScoringLocation("leftside_hub", new Pose2d(13,3, new Rotation2d()), null);
+        scoringLocationsRaw[0] = new ScoringLocation("hub", new Pose2d(13,4, new Rotation2d()), null);
+        scoringLocationsRaw[1] = new ScoringLocation("left_corner", new Pose2d(16,0.5, new Rotation2d()), null);
+        scoringLocationsRaw[2] = new ScoringLocation("climber", new Pose2d(14.5,4, new Rotation2d()), null);
+        scoringLocationsRaw[3] = new ScoringLocation("right_corner", new Pose2d(16,7.5, new Rotation2d()), null);
+        scoringLocationsRaw[4] = new ScoringLocation("rightside_hub", new Pose2d(13,5, new Rotation2d()), null);
+        scoringLocationsRaw[5] = new ScoringLocation("leftside_hub", new Pose2d(13,3, new Rotation2d()), null);
 
 
+        DriverStation.Alliance alliance = DriverStation.getAlliance().get();
 
+        for (int i = 0; i < scoringLocationsRaw.length; i++) {
+            if(alliance == DriverStation.Alliance.Red){
+                ScoringLocation scoringLocation = scoringLocationsRaw[i];
 
-        return scoringLocations;
+                scoringLocations[i] = new ScoringLocationFinalized(scoringLocation.name, scoringLocation.redPose2d);
+            }
+            else if(alliance == DriverStation.Alliance.Blue){
+                ScoringLocation scoringLocation = scoringLocationsRaw[i];
+
+                scoringLocations[i] = new ScoringLocationFinalized(scoringLocation.name, scoringLocation.bluePose2d);
+            }
+        }
+
     }
 
 
@@ -32,6 +45,13 @@ public class ScoringLocationLookup {
         public String name;
         public Pose2d redPose2d;
         public Pose2d bluePose2d;
+
+    }
+
+    @AllArgsConstructor
+    static class ScoringLocationFinalized {
+        public String name;
+        public Pose2d finalizedPose2d;
 
     }
 
