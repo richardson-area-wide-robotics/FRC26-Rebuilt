4b9a27ea39ea4bcd0431d9a6e61b344602cff5ac
at384752 <122325349+at384752@users.noreply.github.com>
2026-03-21 15:11:13 -0500

    Button that does everything

--- src/main/java/frc/robot/rebuilt/RebuiltContainer.java
+++ src/main/java/frc/robot/rebuilt/RebuiltContainer.java
@@ -196,8 +196,13 @@ public class RebuiltContainer implements IRobotContainer {
     // Driver Right Trigger - Load (fire balls if shooter is on)
     RobotUtils.bindControl(
             HIDConstants.DRIVER_CONTROLLER.rightTrigger(),
-            Commands.runOnce(FEEDER::load).alongWith(Commands.runOnce(FEEDER::cycle).alongWith(INTAKE.jiggleItALittleCommand()).alongWith(INTAKE.intakeCommand())),
-            Commands.runOnce(FEEDER::stopLoad).alongWith(Commands.runOnce(FEEDER::stopCycle).alongWith(INTAKE.stopIntakeCommand()).alongWith(INTAKE.stopIntakeCommand())));
+            Commands.runOnce(FEEDER::load)
+              .alongWith(Commands.runOnce(FEEDER::cycle)
+              .alongWith(INTAKE.jiggleItALittleCommand())
+              .alongWith(INTAKE.intakeCommand())),
+            Commands.runOnce(FEEDER::stopLoad)
+              .alongWith(Commands.runOnce(FEEDER::stopCycle)
+              .alongWith(INTAKE.stopIntakeCommand())));
 
     // Driver Left Trigger - Intake
     RobotUtils.bindControl(HIDConstants.DRIVER_CONTROLLER.leftTrigger(),
