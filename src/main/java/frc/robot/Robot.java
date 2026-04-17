3f2e111c084bf35e497c59c9779b9b79fb667cb0
strubium <113206902+strubium@users.noreply.github.com>
2026-04-17 13:07:27 -0500

    robot init auton init

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import com.strubium.ssjprofiler.Profiler;
 import com.strubium.ssjprofiler.ProfilerGlobal;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -43,6 +44,9 @@ public class Robot extends LoggedRobot {
 
   @Override
   public void robotInit() {
+    Profiler profiler = new Profiler("robot init");
+    profiler.start();
+
     try{
       PurpleManager.initialize(
         this,
@@ -71,7 +75,8 @@ public class Robot extends LoggedRobot {
     robotContainer = RobotContainerRegistry.createContainerForTeam(TeamUtils.getTeamNumber());
 
     robotContainer.robotInit();
-}
+    profiler.end();
+  }
 
 
   @Override
@@ -102,6 +107,8 @@ public class Robot extends LoggedRobot {
 
   @Override
   public void autonomousInit() {
+    Profiler profiler = new Profiler("auton init");
+    profiler.start();
     robotContainer.autonomousInit();
 
     autonomousCommand = robotContainer.getAutonomousCommand();
@@ -111,6 +118,7 @@ public class Robot extends LoggedRobot {
 
       CommandScheduler.getInstance().schedule(autonomousCommand);
     }
+    profiler.end();
   }
 
   @Override
