51ec468fe6931871e13e32322603bd785501000f
strubium <113206902+strubium@users.noreply.github.com>
2026-04-23 16:49:04 -0500

    final

--- src/main/java/frc/robot/common/components/hardware/swerve/PIDValue.java
+++ src/main/java/frc/robot/common/components/hardware/swerve/PIDValue.java
@@ -4,9 +4,9 @@ import lombok.Getter;
 
 public class PIDValue {
 
-    @Getter private double kP;
-    @Getter private double kI;
-    @Getter private double kD;
+    @Getter private final double kP;
+    @Getter private final double kI;
+    @Getter private final double kD;
 
     public PIDValue(double kP, double kI, double kD) {
         this.kP = kP;
