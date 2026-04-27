68877f7109c40c6227c82e581197d227d11debf7
at384752 <at384752@g.risd.org>
2026-04-27 13:38:39 -0500

    works i guess

--- src/main/java/frc/robot/common/subsystems/drive/SwerveDriveSubsystem.java
+++ src/main/java/frc/robot/common/subsystems/drive/SwerveDriveSubsystem.java
@@ -13,14 +13,11 @@ import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Robot;
 import frc.robot.common.components.RobotUtils;
 import frc.robot.common.components.hardware.swerve.MAXSwerveDrivetrain;
 import frc.robot.CommonConstants.DriveConstants;
-import frc.robot.common.interfaces.IRobotContainer;
-
 import java.util.function.DoubleSupplier;
 
 public class SwerveDriveSubsystem extends SubsystemBase implements ModulePositionSupplier {
